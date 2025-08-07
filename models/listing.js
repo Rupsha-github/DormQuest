46bc12a677808804eaebed8e5d7cfb20475b582b
Rupsha-github <rupshasaha3002@gmail.com>
2025-08-07 14:01:49 +0530

    schema changes made for showing listings

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
     title: {
@@ -10,14 +11,28 @@ const listingSchema = new Schema({
     image: {
         url: String,
         filename: String
-        // type: String,
-        // default: "https://plus.unsplash.com/premium_photo-1679088034974-2c9c01d59992?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        // set: (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1679088034974-2c9c01d59992?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
     },
     price: Number,
     location: String,
-    country: String
+    country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ],
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
 });
+
+//handles reviews deletion for a listing if the corresponding listing is deleted
+listingSchema.post("findOneAndDelete", async(listing) => {
+    if(listing) {
+        await Review.deleteMany({_id: {$in: listing.reviews}});
+    }
+})
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
