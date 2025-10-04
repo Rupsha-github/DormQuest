cf4bc9f000fefc70ac744fab2b69f59d0d7cddb9
Rupsha-github <rupshasaha3002@gmail.com>
2025-10-05 00:28:00 +0530

    Deployment changes

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [{
+    name: 'dormquest',
+    script: './app.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
